8007256768c786e0e9235057ad9fba0025de5552
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-04-30 10:46:17 +0900

    create project

---
+++ src/main/java/me/kimchidev/springsecondapilogin/SpringSecondApiLoginApplication.java
@@ -0,0 +1,13 @@
+package me.kimchidev.springsecondapilogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecondApiLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecondApiLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kimchidev/springsecondapilogin/SpringSecondApiLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kimchidev.springsecondapilogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecondApiLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
